97b5a68c1892426c6bdba6ee2e7497845d91cf87
wiwi081 <aleksei5555578@mail.ru>
2021-11-02 14:33:15 +0300

    calc debag

---
+++ src/com/company/Main.java
@@ -0,0 +1,120 @@
+package com.company;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+public class Main {
+    static Scanner scanner = new Scanner(System.in);
+    static int number1, number2;
+    static char operation;
+    static int result;
+
+    public static void main (String[] args) {
+        System.out.println("Введите выражение [2+2] или два римских числа от I до X:[V+V] + Enter ");
+//      Считываем строку userInput которую ввёл пользователь
+        String userInput = scanner.nextLine();
+//      Создаём пустой символьный массив длиной 10 символов:  under_char
+        char[] under_char = new char[10];
+//      Заполняем символьный массив символами строки которую ввел пользователь и по ходу ловим знак операции
+        for (int i = 0; i < userInput.length(); i++) {
+            under_char[i] = userInput.charAt(i);
+            if (under_char[i] == '+') {
+                operation = '+';
+            }
+            if (under_char[i] == '-') {
+                operation = '-';
+            }
+            if (under_char[i] == '*') {
+                operation = '*';
+            }
+            if (under_char[i] == '/') {
+                operation = '/';
+            }
+        }
+        String under_charString = String.valueOf(under_char);
+        String[] blacks = under_charString.split("[+-/*]");
+        String stable00 = blacks[0];
+        String stable01 = blacks[1];
+        String string03 = stable01.trim();
+        number1 = romanToNumber(stable00);
+        number2 = romanToNumber(string03);
+        if (number1 < 0 && number2 < 0) {
+            result = 0;
+        } else {
+            result = calculated(number1, number2, operation);
+            System.out.println("---Результат для римских цифр----");
+            String resultRoman = convertNumToRoman(result);
+            System.out.println(stable00 + " " + operation + " " + string03 + " = " + resultRoman);
+        }
+        number1 = Integer.parseInt(stable00);
+        number2 = Integer.parseInt(string03);
+        result = calculated(number1, number2, operation);
+        System.out.println("--Результат для арабских цифр----");
+        System.out.println(number1 + " " + operation + " " + number2 + " = " + result);
+    }
+
+    private static String convertNumToRoman (int numArabian) {
+        String[] roman = {"O", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
+        return roman[numArabian];
+    }
+
+
+    private static int romanToNumber (String roman) {
+        try {
+            switch (roman) {
+                case "I":
+                    return 1;
+                case "II":
+                    return 2;
+                case "III":
+                    return 3;
+                case "IV":
+                    return 4;
+                case "V":
+                    return 5;
+                case "VI":
+                    return 6;
+                case "VII":
+                    return 7;
+                case "VIII":
+                    return 8;
+                case "IX":
+                    return 9;
+                case "X":
+                    return 10;
+            }
+        } catch (InputMismatchException e) {
+            throw new InputMismatchException("Неверный формат данных");
+        }
+        return -1;
+    }
+
+    public static int calculated (int num1, int num2, char op) {
+        int result = 0;
+        switch (op) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                try {
+                    result = num1 / num2;
+                } catch (ArithmeticException | InputMismatchException e) {
+                    System.out.println("Exception : " + e);
+                    System.out.println("Only integer non-zero parameters allowed");
+
+                    break;
+                }
+                break;
+            default:
+                throw new IllegalArgumentException("Не верный знак операции");
+        }
+        return result;
+    }
+}
